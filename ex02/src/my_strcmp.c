7f9bf0960cef4f6c0da8cc7affb7d2ace5b6d82a
Anna Kulakova <annkulakova7@gmail.com>
2019-08-05 13:47:02 +0300

    Ex.2 is finished

---
+++ ex02/src/my_strcmp.c
@@ -0,0 +1,11 @@
+#include "test.h"
+
+int  my_strcmp(char *str1, char *str2)
+{
+    for( ; *str1 == *str2; str1++, str2++)
+    {
+         if (*str1 == '\0')
+         return(0);
+    }
+    return(*str1 - *str2);
+} 
