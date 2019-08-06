8a47e909e43b37a5c914d2909d3519dff7ebf511
Danylo Patsora <dpatsora@gmail.com>
2019-08-06 17:23:05 +0300

    Finish ex05

---
+++ ex05/src/my_puts.c
@@ -0,0 +1,15 @@
+
+#include "test.h"
+#include <stdlib.h>
+#include <unistd.h>
+int my_puts(const char *s)
+{
+  int i = 0;
+	while (s[i]!='\0')
+  {
+	  write(1, &s[i], 1);
+	  i++; 
+	}
+  write (1, "\n", 1);
+  return 0;
+}
