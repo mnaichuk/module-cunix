8793cab47af12529e76a202419270cd581564e7a
Anna Kulakova <annkulakova7@gmail.com>
2019-08-07 18:43:09 +0300

    Ex.8 is finished

---
+++ ex08/src/binary_tree.c
@@ -0,0 +1,81 @@
+#include "../include/binary_tree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+node_t *allocnode()
+{
+node_t 	*new;
+new = (node_t *) malloc(sizeof(node_t));
+new->left = NULL;
+new->right = NULL;
+return new;
+}
+node_t *insert(node_t *root, char *key, void *data)
+{
+    if(root == NULL)
+    {
+		root = allocnode();
+		root->key = key;
+		root->data = data;
+   	}
+    	else if(root->key >= key)
+     		root->right = insert(root->right, key, data);
+    	else
+	    	root->left = insert(root->left, key, data);
+    	return root;
+}
+void print_node(node_t *node)
+{
+	if (node != NULL)
+		printf("%p\n", node->data);
+ 	else
+	 	return;
+}
+void visit_tree(node_t *node, void (*fp)(node_t *root))
+{
+  	if(node->right != NULL)
+  	{
+    	if(node->left != NULL)
+    	{
+      		visit_tree(node->left, fp);
+      		visit_tree(node->right, fp);
+        }
+    		else
+    	{
+        	visit_tree(node->right, fp);
+    	}
+  	}
+  	else
+  	{
+    	if(node->left != NULL)
+    	{
+      	    visit_tree(node->left, fp);
+    	}
+  	}
+  	fp(node);
+}
+void destroy_tree(node_t *node, void (*fdestroy)(node_t *root))
+{
+  	if(node->right != NULL)
+  	{
+    	if(node->left != NULL)
+    	{
+      			destroy_tree(node->left, fdestroy);
+      			destroy_tree(node->right, fdestroy);
+    	}
+    	else
+    	{
+        		destroy_tree(node->right, fdestroy);
+    	}	
+  	}
+  	else
+  	{
+     		if(node->left != NULL)
+    		{
+      			destroy_tree(node->left, fdestroy);
+    		}
+  	}
+	fdestroy(node);
+	free(node);
+}
